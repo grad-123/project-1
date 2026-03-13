d3b8e4a5ee9cd0334a0c93352cd0e8c8eb2a7235
Masa Omar <mmash7692@gmail.com>
2026-03-13 19:05:07 +0300

    جديد

--- src/component/NotFound/NotFound.jsx
+++ src/component/NotFound/NotFound.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import "./NotFound.css";
 
 function NotFound() {
+  const { t } = useTranslation();
+
   return (
-    <div>NotFound</div>
-  )
+    <div className="notfound">
+      <h1>404</h1>
+      <h2>{t("found.page")}</h2>
+      <p>{t("found.sorry")}</p>
+
+      <Link to="/" className="home-btn">
+        {t("found.go")}
+      </Link>
+    </div>
+  );
 }
 
-export default NotFound
+export default NotFound;
